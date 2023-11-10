01f2d2f3c27430f8937341de15fd2b5a114b6c9a
Ahmad Rezazade <ahmad1785@gmail.com>
2023-11-10 14:41:47 -0800

    first commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,5 @@
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-#gunicorn engine.wsgi:application --bind 0.0.0.0:8000
 
 gunicorn engine.wsgi:application --bind 0.0.0.0 --workers 3 --timeout 1800
